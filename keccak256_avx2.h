8ad2120cad736d204ce30169c3a477421319ea71
8891689 <8891689@protonmail.com>
2025-08-20 02:50:34 +0800

    Update and rename keccak256_avx.h to keccak256_avx2.h

--- keccak256_avx2.h
+++ keccak256_avx2.h
@@ -1,7 +1,21 @@
-//Author: 8891689
-//https://github.com/8891689
-#ifndef KECCAK256_AVX_H
-#define KECCAK256_AVX_H
+/* Apache License, Version 2.0
+   Copyright [2025] [8891689]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   Author: 8891689 (https://github.com/8891689)
+*/
+#ifndef KECCAK256_AVX2_H
+#define KECCAK256_AVX2_H
 
 #include <stdint.h>
 #include <immintrin.h> 
@@ -34,4 +48,4 @@ void keccak_absorb_8blocks_avx8(KECCAK_CTX_AVX8 *ctx, const uint8_t input_data_8
 void keccakf_transform_avx8(KECCAK_CTX_AVX8 *ctx); // 8 个并行状态的核心排列
 void keccak_extract_hash_8lanes_avx8(const KECCAK_CTX_AVX8 *ctx, uint8_t output_hashes_8lanes[8][KECCAK_HASH_BYTES]);
 
-#endif // KECCAK256_AVX_H
+#endif // KECCAK256_AVX2_H
